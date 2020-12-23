1e564801e2c71740a91d3fc0ddd404564091cbcc
Zazas <bran12chembah@outlook.com>
2020-12-23 16:15:56 -0500

    created header and navbar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import Header from "./headernav/header";
+import Navbar from "./headernav/navbar";
 
 class Layout extends Component {
   render() {
-    return <div className="app">{this.props.children}</div>;
+    return (
+      <div className="layout">
+        {this.props.children}
+        <Header />
+        <Navbar />
+      </div>
+    );
   }
 }
 export default Layout;
